7a3fa286c913cb455f3c54c6ced91b5e201f2fe6
Zakhar <zkostyutkin2004@gmail.com>
2026-01-11 16:40:13 +0500

    saturn V scenario

---
+++ Assets/Scripts/Core/Museum/Scenarios/SaturnVRocket.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaturnVRocket : BaseMuseumPanel
+{
+    public override void ActivateScenario()
+    {
+        
+    }
+
+    protected override void OnButtonPressedLogic()
+    {
+        
+    }
+}
